116c8f7f7989bc09396cdd4b1859ba5e30c5e213
Maharaj Dey <maharajdey23@gmail.com>
2023-04-05 10:27:51 +0530

    ed

--- app/src/main/java/com/example/woodpeaker/Notification.kt
+++ app/src/main/java/com/example/woodpeaker/Notification.kt
@@ -10,3 +10,12 @@ class Notification(to:String,title:String,body:String) {
         notification.setData(title,body)
     }
 }
+class NotificationData() {
+    lateinit var title:String
+    lateinit var body:String
+
+    fun setData(title:String,body:String){
+        this.title=title
+        this.body=body
+    }
+}
